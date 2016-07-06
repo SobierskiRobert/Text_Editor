b4073dedc7ac7f1849f26163a8a93e28b5c61bed
Robert Sobierski <sobierski.robert@gmail.com>
2016-07-06 11:24:43 +0200

    add bold, underline, italic, font-family, font-size

--- Text_Editor/MainWindow.xaml.cs
+++ Text_Editor/MainWindow.xaml.cs
@@ -1,8 +1,13 @@
-﻿
+﻿using System.Collections.Generic;
+using System.Linq;
+
 using System.Windows;
+using System.Windows.Controls;
 
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
+
 using System.IO;
 using Microsoft.Win32;
 
@@ -16,6 +21,23 @@ namespace Text_Editor
         public MainWindow()
         {
             InitializeComponent();
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+        }
+
+        private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            object temp = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            btnUnderline.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(TextDecorations.Underline));
+
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            cmbFontFamily.SelectedItem = temp;
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty);
+            cmbFontSize.Text = temp.ToString();
         }
 
         private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
@@ -41,6 +63,17 @@ namespace Text_Editor
                 range.Save(fileStream, DataFormats.Rtf);
             }
 
+        }
+
+        private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbFontFamily.SelectedItem != null)
+                rtbEditor.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+        }
+
+        private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
         }
     }
 }
